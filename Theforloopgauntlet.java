8a8759d12f93d4e33d912c16d5437253af496714
kaylarwright <kayla.wright@gompersstudent.org>
2017-07-24 14:57:05 -0700

    Add files via upload

---
+++ Theforloopgauntlet.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.SynchronousQueue;
+
+public class Theforloopgauntlet {
+public static void main(String[] args) {
+	
+
+	//for (int i = 0; i < 101; i++) {
+		//System.out.println(i);
+	//}
+	//for (int i = 100; i > 0; i--) {
+		//System.out.println(i);
+		
+	//}
+	//for (int i = 2; i < 101; i += 2) {
+		//System.out.println(i);
+	//}
+	//for (int i = 1; i < 100; i +=2) {
+		//System.out.println(i);
+	//}
+	//for (int i = 1; i < 501; i++) {
+		//if(i % 2 == 0){
+			//System.out.println(i + "even");
+			
+		//}else{
+			//System.out.println(i + "odd");
+		//}
+	//}
+	//for (int i = 0; i < 778; i++) {
+		//if(i % 7 == 0){
+			//System.out.println(i);
+		//}
+	//}
+	//for (int i = 0; i < 15; i++) {
+			//System.out.println("In " + (2003 + i) + " I was " + i + " years old");
+		//}
+		for (int i = 0; i < args.length; i++) {
+			for (int j = 0; j < args.length; j++) {
+				
+			}
+		}	
+}
+}
